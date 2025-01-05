528790923eb119a9b9c50f5e2d8c9c6c11c881a1
moronturtle <hilmangp@gmail.com>
2025-01-05 22:25:48 +0700

    added cart icon and delete icon in navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,32 +1,54 @@
 import Image from "next/image";
-import './style.scss';
+import "./style.scss";
 
 const Navbar = () => {
   return (
     <header className="header">
-    <div className="header-container">
-      <div className="header-logo">
-        <Image
-          src="/assets/logo/header-logo.svg"
-          alt="Logo"
-          width={64}
-          height={64}
-        />
+      <div className="header-container">
+        <div className="header-logo">
+          <Image
+            src="/assets/logo/header-logo.svg"
+            alt="Logo"
+            width={64}
+            height={64}
+          />
+        </div>
       </div>
-    </div>
-    <div className="find-category">
-      <div className="find-category-searh-input">
-        <input className="input-size-lg" type="text" placeholder="Cari Kategori" />
+      <div className="find-category">
+        <div className="find-category-searh-input">
+          <input
+            className="input-size-lg"
+            type="text"
+            placeholder="Cari Kategori"
+          />
+          <Image
+            src="/assets/icon/search.svg"
+            className="find-category-searh-logo"
+            alt="Logo"
+            width={30}
+            height={30}
+          />
+        </div>
+      </div>
+      <div className="cart-pos">
+        <div className="container-cart">
+          <Image
+            src="/assets/icon/cart.svg"
+            alt="Logo"
+            width={30}
+            height={30}
+          />
+          <div className="badge">5</div>
+        </div>
         <Image
-          src="/assets/icon/search.svg"
-          className="find-category-searh-logo"
+          className="pointer"
+          src="/assets/icon/delete.svg"
           alt="Logo"
           width={30}
           height={30}
         />
       </div>
-    </div>
-  </header>
+    </header>
   );
 };
 
