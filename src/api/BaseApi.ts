7465a846dd9d42fbb0d00c4a6ce33fd01b319bd8
moronturtle <hilmangp@gmail.com>
2025-01-05 10:50:26 +0700

    added global api function

---
+++ src/api/BaseApi.ts
@@ -0,0 +1,51 @@
+import axios, { AxiosResponse } from "axios";
+
+interface CallOptions {
+  method: "GET" | "POST" | "PUT" | "DELETE";
+  url: string;
+  data?: Record<string, any>;
+  queryParams?: Record<string, any>;
+  overrideBaseUrl?: string;
+  headers?: Record<string, string>;
+  options?: Record<string, any>;
+}
+
+export async function call({
+  method,
+  url,
+  data = {},
+  queryParams = {},
+  overrideBaseUrl,
+  headers,
+  options,
+}: CallOptions): Promise<AxiosResponse<any>> {
+  if (!url) {
+    throw new Error("URL is required.");
+  }
+
+  let newUrl = overrideBaseUrl || "";
+  const queryString = new URLSearchParams(
+    Object.fromEntries(
+      Object.entries(queryParams).filter(
+        ([_, data]) => data !== undefined && data !== null && data !== ""
+      )
+    )
+  ).toString();
+
+  const fullUrl = `${url}?${queryString}`;
+
+  try {
+    const response = await axios({
+      method,
+      url: fullUrl,
+      baseURL: newUrl,
+      data: method !== "GET" ? data : undefined, // Only send `data` for non-GET requests
+      headers,
+      ...options,
+    });
+    return response;
+  } catch (error) {
+    console.error("Error making API call:", error);
+    throw error;
+  }
+}
